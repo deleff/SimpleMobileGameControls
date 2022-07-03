6fd584f109f80fa40340328736d797fe945d77ce
yitzchake <yitzchake@reblaze.com>
2022-07-03 22:37:51 +0300

    touch screen buttons

--- MobileGame/scenes/CharacterSelectScene.gd
+++ MobileGame/scenes/CharacterSelectScene.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var player_1: String = ""
 
 # Called when the node enters the scene tree for the first time.
@@ -11,30 +7,24 @@ func _ready():
 	$AudioStreamPlayer2D.play()
 	$Label/MegamanIcon.connect("pressed", self, "_on_megaman_selected")
 	$Label/RollIcon.connect("pressed", self, "_on_roll_selected")
+	$StartGame.set_block_signals(true)
+	$TrainingMode.set_block_signals(true)
+
 
 func _on_megaman_selected():
 	$Label/CharacterLogo.texture = load("res://characters/heroes/megaman/sprites/logo.png")
 	player_1 = "Megaman"
 	print("Player 1: ", player_1)
-	$TrainingMode.disabled = false
+	$TrainingMode.set_block_signals(false)
 	$TrainingMode.visible = true
-	$StartGame.disabled = false
+	$StartGame.set_block_signals(false)
 	$StartGame.visible = true
 
 func _on_roll_selected():
 	$Label/CharacterLogo.texture = load("res://characters/heroes/roll/sprites/logo.png")
 	player_1 = "Roll"
 	print("Player 1: ", player_1)
-	$TrainingMode.disabled = false
+	$TrainingMode.set_block_signals(false)
 	$TrainingMode.visible = true
-	$StartGame.disabled = false
+	$StartGame.set_block_signals(false)
 	$StartGame.visible = true
-
-
-func _input(event):
-	if event is InputEventScreenTouch && event.is_pressed():
-		pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
